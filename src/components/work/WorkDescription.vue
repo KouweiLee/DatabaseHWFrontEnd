575a70e104654f7ec8e83bb7c30a257ff1c29c54
Pentium <1793030808@qq.com>
2022-11-16 19:56:55 +0800

    zkg11_16作业描述页面，作业管理页面

--- src/components/work/WorkDescription.vue
+++ src/components/work/WorkDescription.vue
@@ -1,15 +1,39 @@
 <template>
-    <h1>{{ work.name }}</h1>
-    <mavon-editor
-        ref="md"
-        v-model="work.content"
-        :subfield="false"
-        :defaultOpen="'preview'"
-        :toolbarsFlag="false"
-        :editable="false"
-        :scrollStyle="true"
-        :ishljs="true"
-    />
+    <h2>{{ work.name }}</h2>
+    <el-divider style="margin-left: 10%; width: 80%"/>
+    <el-row style="margin-left: 10%; margin-top: 30px; width: 80%">
+        <el-col :span="8">
+            <el-row><h3>开始时间</h3></el-row>
+            <el-row><h3>{{ work.begin_time.replace('T', ' ') }}</h3></el-row>
+        </el-col>
+        <el-col :span="8">
+            <el-row><h3>结束时间</h3></el-row>
+            <el-row><h3>{{ work.end_time.replace('T', ' ') }}</h3></el-row>
+        </el-col>
+        <el-col :span="8">
+            <el-row><h3>剩余时间</h3></el-row>
+            <el-row><h3>{{getRemainTime(work.end_time)}}</h3></el-row>
+        </el-col>
+    </el-row>
+    <el-row style="margin-left: 10%; width: 80%">
+        <el-col :span="24" style="margin: 0 0 0 0">
+            <el-progress :percentage="timeDifference(work.begin_time, work.end_time)" :color="colors" :show-text="false"/>
+        </el-col>
+    </el-row>
+    <el-row style="margin-left: 5%; margin-top:30px; width: 90%">
+        <el-col :span="24">
+            <mavon-editor
+                ref="md"
+                v-model="work.content"
+                :subfield="false"
+                :defaultOpen="'preview'"
+                :toolbarsFlag="false"
+                :editable="false"
+                :scrollStyle="true"
+                :ishljs="true"
+            />
+        </el-col>
+    </el-row>
 </template>
 
 <script>
@@ -21,6 +45,13 @@ import API from "../../axios.js"
 export default {
     name: "WorkDescription",
     setup() {
+        const colors = [
+          { color: '#f56c6c', percentage: 20 },
+          { color: '#e6a23c', percentage: 40 },
+          { color: '#5cb87a', percentage: 60 },
+          { color: '#1989fa', percentage: 80 },
+          { color: '#6f7ad3', percentage: 100 },
+        ]
         let work = reactive({
             id: 1,
             name: "c1",
@@ -28,8 +59,9 @@ export default {
             begin_time: "2017-07-25 21:51:54",
             end_time: "2017-07-25 21:51:54"
         })
-let route = useRoute();
+        let route = useRoute();
         getWork()
+
         //获取当前课程信息
         function getWork() {
             API.post(API.defaults.baseUrl + '/course/work/single/', {id: route.query.id})
@@ -38,7 +70,7 @@ let route = useRoute();
                         work.id = response.data.data.id
                         work.begin_time = response.data.data.begin_time
                         work.end_time = response.data.data.end_time
-                        work.content = response.data.data.content === null? "": response.data.data.content
+                        work.content = response.data.data.content === null ? "" : response.data.data.content
                         work.name = response.data.data.name
 
                     }
@@ -52,11 +84,41 @@ let route = useRoute();
             return STORE.state.isSuperUser;
         }
 
+        function timeDifference(beginTime, endTime) {
+            var dateBegin = new Date(beginTime)
+            var dateEnd = new Date(endTime)
+            var totalTimeSpan = dateEnd.getTime() - dateBegin.getTime();
+            var dateNow = new Date()
+            var nowTimeSpan = dateEnd.getTime() - dateNow.getTime()
+            console.log(dateBegin)
+            console.log(dateNow)
+            console.log(dateEnd)
+            console.log(totalTimeSpan)
+            console.log(nowTimeSpan)
+            var percentage = 100 * nowTimeSpan / totalTimeSpan
+            return percentage;
+        }
+
+        function getRemainTime(endTime) {
+            var dateEnd = new Date(endTime)
+            var dateNow = new Date()
+            var nowTimeSpan = dateEnd.getTime() - dateNow.getTime()
+            console.log(dateNow)
+            console.log(dateEnd)
+            console.log(nowTimeSpan)
+            var dayDiff = Math.floor(nowTimeSpan / (24 * 3600 * 1000));
+            var leave1 = nowTimeSpan % (24 * 3600 * 1000);
+            var hours = Math.floor(leave1 / (3600 * 1000))
+            return dayDiff + "天" + hours + "小时"
+        }
 
         return {
+            colors,
             isSuperUser,
             work,
-            getWork
+            getWork,
+            timeDifference,
+            getRemainTime
         }
     }
 }
