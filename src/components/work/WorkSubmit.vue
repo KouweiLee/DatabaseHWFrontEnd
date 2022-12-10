58d44e1142bf2a59298649318dd3c973c9dde176
李佳禄 <20373110@buaa.edu.cn>
2022-12-10 10:20:29 +0800

    12.10_1

--- src/components/work/WorkSubmit.vue
+++ src/components/work/WorkSubmit.vue
@@ -93,7 +93,7 @@
             <i class="el-icon-upload"></i>
             <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
             <template #tip>
-                <div class="el-upload__tip">不超过 500kb</div>
+                <div class="el-upload__tip">在此处上传作业附件</div>
             </template>
         </el-upload>
     </el-row>
